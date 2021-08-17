27ac589b66254048ea789fa7f1e886edeb4e242c
Yohan <yohan@yohanpro.com>
2021-08-17 22:56:36 +0900

    Feat: Hot리로딩, dotenv 파일 전역 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 8080;
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  Logger.log(`Server is now runnig on ${port}`, 'Bootstarp');
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  await app.listen(port);
 }
 bootstrap();
