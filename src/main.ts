481133fc4fe9cb71f0410a6de1265c2384697f19
Yohan <yohan@yohanpro.com>
2021-08-22 10:06:30 +0900

    Feat: Swagger api 적용

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 8080;
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('동행 API 문서')
+    .setDescription('동행 API 문서')
+    .setVersion('1.0')
+    .addTag('동행')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   Logger.log(`Server is now runnig on ${port}`, 'Bootstarp');
 
