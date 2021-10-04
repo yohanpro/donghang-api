d622dee5a50f85846a4ac1ddb6c0849ef82ca736
Yohan <yohan@yohanpro.com>
2021-10-04 14:39:01 +0900

    feat: procfile

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 import session from 'express-session';
 
 // const port = process.env.PORT || 8080;
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 declare const module: any;
 
 async function bootstrap() {
