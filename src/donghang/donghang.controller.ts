b9521045f44913e13df58ea80b19a8f54e09cd22
Yohan <yohan@yohanpro.com>
2021-10-09 20:38:29 +0900

    feat: donghang by countryId Swagger 작성

--- src/donghang/donghang.controller.ts
+++ src/donghang/donghang.controller.ts
@@ -1,5 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { DonghangService } from './donghang.service';
 
@@ -7,12 +12,12 @@ import { DonghangService } from './donghang.service';
 @UseGuards(new JwtAuthGuard())
 export class DonghangController {
   constructor(private readonly donghangService: DonghangService) {}
-
+  @ApiParam({ name: 'countryId', description: 'ISO 국가코드', required: true })
   @ApiTags('나라별 동행 글 전체 가져오기')
   @ApiOperation({ summary: '나라별 동행 전체 글' })
   @ApiBearerAuth()
-  @Get()
-  getDonghangArticleByCountry() {
-    return 'donghang article';
+  @Get(':countryId')
+  getDonghangArticleByCountry(@Param('countryId') countryId: number) {
+    return `donghang article,`;
   }
 }
