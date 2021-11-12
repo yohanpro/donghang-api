23dcc0d757ba2b77490fd233722016ab0f5d8c87
Yohan <yohan@yohanpro.com>
2021-11-12 22:48:45 +0900

    feat: service에 repository 주입

--- src/donghang/donghang.module.ts
+++ src/donghang/donghang.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DonghangArticle } from 'src/entities/DonghangArticle';
+import { DonghangComments } from 'src/entities/DonghangComments';
 import { DonghangController } from './donghang.controller';
 import { DonghangService } from './donghang.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DonghangComments, DonghangArticle])],
   providers: [DonghangService],
   controllers: [DonghangController],
 })
--- src/donghang/donghang.service.ts
+++ src/donghang/donghang.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DonghangArticle } from 'src/entities/DonghangArticle';
+import { DonghangComments } from 'src/entities/DonghangComments';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DonghangService {
+  constructor(
+    @InjectRepository(DonghangComments)
+    private commentsRepository: Repository<DonghangComments>,
+
+    @InjectRepository(DonghangArticle)
+    private articleRepository: Repository<DonghangArticle>,
+  ) {}
+
   getCountryDongHangArticle({ countryId, page = 0, size = 5 }): string {
     return `${countryId} donghang page: ${page}, size: ${size}`;
   }
